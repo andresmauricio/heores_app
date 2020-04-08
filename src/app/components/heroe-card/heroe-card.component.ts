7bbda0b58989a50a54ed42454d061d910675b9d1
andresmauricio <andresacelas@outlook.com>
2020-04-07 22:09:59 -0500

    comunicacion hijo a padre y correcion busqueda

--- src/app/components/heroe-card/heroe-card.component.ts
+++ src/app/components/heroe-card/heroe-card.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Heroe } from 'src/app/models/heroes';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-heroe-card',
@@ -8,9 +9,18 @@ import { Heroe } from 'src/app/models/heroes';
 export class HeroeCardComponent implements OnInit {
   @Input() heroe: Heroe;
   @Input() index: number;
-  constructor() { }
+  @Output() heroSelected: EventEmitter<number>;
+
+  constructor(private router: Router) { 
+    this.heroSelected = new EventEmitter();
+  }
 
   ngOnInit() {
+  }
+
+  public emitToIndexHero() {
+    this.router.navigate(['/detalle-heroe', this.index]);
+    // this.heroSelected.emit(this.index); Evento para comunicacion desde el hijo hasta el padre 
   }
 
 }
--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService } from 'src/app/services/heroes.service';
 import { Heroe } from 'src/app/models/heroes';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-heroes',
@@ -10,10 +11,14 @@ import { Heroe } from 'src/app/models/heroes';
 export class HeroesComponent implements OnInit {
   public heroes: Heroe[];
 
-  constructor(private heroesService: HeroesService) { }
+  constructor(private heroesService: HeroesService, private router: Router) { }
 
   ngOnInit() {
     this.heroes = this.heroesService.getHeroes();
+  }
+
+  public goTohero(idx: number) {
+    this.router.navigate(['/detalle-heroe', idx]);
   }
 
 }
